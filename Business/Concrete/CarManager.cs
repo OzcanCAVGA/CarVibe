c245430fbc3c2c358af4909aa5758cb2b111cee1
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2024-08-18 18:55:59 +0300

    Added Brand and Color entities, set up related SQL Server tables, and implemented a generic IEntityRepository. Configured Entity Framework for Car, Brand, and Color, and developed services for filtering cars by BrandId and ColorId. Validated car names to have at least 2 characters and ensured daily prices are above 0.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Abstract;
 using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFrameworkk;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -16,12 +17,36 @@ namespace Business.Concrete
         {
             _carDal = carDal;
         }
+
+        public void Add(Car car)
+        {
+            if (car.Description.Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Araba ismi minimum 2 karakter olmalıdır ve günlük fiyatı 0'dan büyük olmalıdır.");
+            }
+        }
+
         public List<Car> GetAll()
         {
             // is kodlari
             // Yetkisi var mi?
             // vs vs
-            return  _carDal.GetAll();
+            return _carDal.GetAll();
+        }
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+
+            return _carDal.GetAll(c => c.BrandID == brandId);
+        }
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+
+            return _carDal.GetAll(c => c.ColorID == colorId);
+
         }
     }
 }
--- ConsolUI/Program.cs
+++ ConsolUI/Program.cs
@@ -2,6 +2,7 @@
 using Business.Concrete;
 using System;
 using Entities.Concrete;
+using DataAccess.Concrete.EntityFrameworkk;
 
 namespace ConsolUI
 {
@@ -10,7 +11,12 @@ namespace ConsolUI
         static void Main(string[] args)
         {
 
-            CarManager carManager = new CarManager(new InMemoryCarDal());
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            Car car1 = new Car { ID = 1, BrandID = 1, ColorID = 1, ModelYear = "2021", DailyPrice = 500, Description = "Audi A3" };
+            
+            carManager.Add(car1);
+
 
             foreach(var car in carManager.GetAll())
             {

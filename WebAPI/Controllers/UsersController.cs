0066b370d457a7ab424c695d1d993482dd7ff674
Ozcan <cavga1997@gmail.com>
2024-10-21 22:31:19 +0300

    feat: add APIs for adding, deleting, updating and geting in UsersController

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Business.Abstract;
+using Business.Constants;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -7,6 +10,66 @@ namespace WebAPI.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        IUserService _userService;
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
 
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _userService.GetAllUsers();
+            if (result == null)
+            {
+                return BadRequest(Messages.NotFound);
+            }
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var result = _userService.GetUserById(id);
+            if (result == null)
+            {
+                return BadRequest(Messages.NotFound);
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest(Messages.NameInvalid);
+            }
+            _userService.AddUser(user);
+            return Ok(user);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateUser(User user) 
+        {
+            var result = _userService.GetUserById(user.ID);
+            if(result.Success)
+            {
+                _userService.UpdateUser(user);
+                return Ok(user);
+            }
+            return BadRequest(Messages.NameInvalid);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteUser(int id)
+        {
+            var result = _userService.GetUserById(id);
+            if(result.Success)
+            {
+                _userService.DeleteUser(result.Data);
+                return Ok(Messages.Deleted); 
+            }
+            return BadRequest(Messages.NotFound);
+        }
     }
 }

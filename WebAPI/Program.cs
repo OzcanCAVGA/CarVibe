ded94fe5ff4a2c9190156d888401fb4a6088e78a
Ozcan <cavga1997@gmail.com>
2024-10-21 22:14:21 +0300

    feat: add APIs for adding, deleting, updating and getting in RentalsController

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -19,6 +19,12 @@ builder.Services.AddSingleton<IBrandDal, EfBrandDal>();
 builder.Services.AddSingleton<ICustomerService, CustomerManager>();
 builder.Services.AddSingleton<ICustomerDal, EfCustomerDal>();
 
+builder.Services.AddSingleton<IRentalService, RentalManager>();
+builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
+
+builder.Services.AddSingleton<IUserService, UserManager>();
+builder.Services.AddSingleton<IUserDal, EfUserDal>();
+
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

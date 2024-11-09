332de8e74e6617fe1a661cb818fbe1966537face
Özcan ÇAVGA <cavga1997@gmail.com>
2024-11-09 17:31:41 +0300

    feat: Integrate FluentValidation package via NuGet for enhanced validation system

---
+++ Business/ValidationRules/FluentValidation/RentalValidator.cs
@@ -0,0 +1,30 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class RentalValidator : AbstractValidator<Rental>
+    {
+        public RentalValidator()
+        {
+            RuleFor(r => r.CarId).NotEmpty(); 
+
+
+            RuleFor(r => r.CustomerId).GreaterThan(0);
+
+            // RentDate boş olamaz ve ReturnDate'ten önce olmalıdır.
+            RuleFor(r => r.RentDate).NotEmpty();
+            RuleFor(r => r.ReturnDate).GreaterThan(r => r.RentDate)
+                .When(r => r.ReturnDate.HasValue); // ReturnDate varsa, RentDate'ten büyük olmalı
+
+            // ReturnDate, RentDate'ten önce olamaz.
+            RuleFor(r => r.ReturnDate).GreaterThan(r => r.RentDate)
+                .When(r => r.ReturnDate.HasValue); // ReturnDate varsa, RentDate'ten büyük olmalı
+        }
+    }
+}
